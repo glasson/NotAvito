c7361134facf37d890f8ee7ca832ab8ef8b2b745
Mikhail <alphaglasson@mail.ru>
2023-04-24 16:06:45 +0700

    блять сделал ресет и все слетело 1

--- notavito/main/views.py
+++ notavito/main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def test(request):
     return render(request, template_name="main/footer.html")
+
+def create_post(req):
+    return render(req, template_name='main/create_post.html')
