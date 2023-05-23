84e0b02a0bc85cb21758a900eef283b5fe6e9222
Mikhail <alphaglasson@mail.ru>
2023-05-23 17:44:31 +0700

    ...

---
+++ notavito/static/main/css/script.js
@@ -0,0 +1,6 @@
+const screenWidth = screen.width;
+const screenHeight = screen.height;
+var body = document.getElementById('body');
+body.width = screenWidth;
+body.height = screenHeight
+new SimpleBar(document.querySelector('.my-container'));
